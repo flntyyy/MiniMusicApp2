56cc1110a740c73fe95aa46588e94e96d4e27ed5
alexandrsmirnov <flntyyy@gmail.com>
2018-01-23 23:41:28 +0300

    first

--- IdeaProjects/first_test/src/test/java/insurance.java
+++ IdeaProjects/first_test/src/test/java/insurance.java
@@ -1,14 +1,17 @@
 
 
-        import java.util.concurrent.TimeUnit;
-        import org.junit.*;
-        import static org.junit.Assert.*;
-        import org.openqa.selenium.*;
-        import org.openqa.selenium.chrome.ChromeDriver;
-        import org.openqa.selenium.support.ui.ExpectedConditions;
-        import org.openqa.selenium.support.ui.Select;
-        import org.openqa.selenium.support.ui.Wait;
-        import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class insurance {
     private WebDriver driver;
@@ -17,7 +20,7 @@ public class insurance {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 
         driver = new ChromeDriver();
         baseUrl = "http://www.sberbank.ru/ru/person";
@@ -35,12 +38,12 @@ public class insurance {
         driver.findElement(By.xpath("//*[contains(text(),'Страхование путешественников')]")).click();
 
         driver.findElement(By.xpath("//img[contains(@src,'id=f6c836e1-5c5c-4367-b0d0-bbfb96be9c53')]")).click();
-        String winHandleBefore = driver.getWindowHandle();
 
-        for(String winHandle : driver.getWindowHandles()){
+
+        for (String winHandle : driver.getWindowHandles()) {
             driver.switchTo().window(winHandle);
         }
-        Wait<WebDriver> wait2 = new WebDriverWait(driver, 5, 1000);
+
         wait.until(ExpectedConditions.visibilityOf(
                 driver.findElement(By.xpath("//*[contains(text(),'Минимальная')]"))));
         driver.findElement(By.xpath("//*[contains(text(),'Минимальная')]")).click();
@@ -63,7 +66,7 @@ public class insurance {
     }
 
 
-    private void fillField(By locator, String value){
+    private void fillField(By locator, String value) {
         driver.findElement(locator).clear();
         driver.findElement(locator).sendKeys(value);
     }
